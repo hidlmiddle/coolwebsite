d5b0f7ab6812fdfa1d51d0e4987b5441d364864c
Matvey Omelchuk <matveyomelchuk@Matveys-iMac.local>
2025-08-06 20:23:19 -0700

    refactor: simplify video background implementation

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import VideoBackground from './VideoBackground';
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,7 +9,18 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      <VideoBackground />
+      {/* Video Background */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10 pointer-events-none brightness-75"
+      >
+        <source src="/videos/Verceland_Glassmorphism_Background_Video.mp4" type="video/mp4" />
+      </video>
+
+      {/* Main Content */}
       <div className="relative z-10 flex flex-col flex-grow">
         <Header />
         <main className="flex-grow">
